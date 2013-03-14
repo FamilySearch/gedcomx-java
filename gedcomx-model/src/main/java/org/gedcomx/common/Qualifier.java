f19a64542f09fa0ac3d6423281c42ac922fed582
Ryan Heaton <heatonra@familysearch.org>
2013-03-14 10:19:49 -0600

    adding the notion of 'qualifiers' and fixing 'namePart' to use it

---
+++ gedcomx-model/src/main/java/org/gedcomx/common/Qualifier.java
@@ -0,0 +1,103 @@
+package org.gedcomx.common;
+
+import org.codehaus.enunciate.XmlQNameEnumUtil;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ * A data qualifier. Qualifiers are used to "qualify" certain data elements to provide additional context, information, or details.
+ *
+ * @author Ryan Heaton
+ */
+@XmlType( name = "Qualifier" )
+public final class Qualifier {
+
+  private URI name;
+  private String value;
+
+  public Qualifier() {
+  }
+
+  public Qualifier(URI name, String value) {
+    setName(name);
+    setValue(value);
+  }
+
+  public Qualifier(URI name) {
+    setName(name);
+  }
+
+  public Qualifier(Enum name, String value) {
+    setName(name);
+    setValue(value);
+  }
+
+  public Qualifier(Enum name) {
+    setName(name);
+  }
+
+  /**
+   * The name of the qualifier. The name should be an element of a constrained vocabulary and is used to determine meaning of the qualifier.
+   *
+   * @return The name of the qualifier.
+   */
+  @XmlAttribute
+  public URI getName() {
+    return name;
+  }
+
+  /**
+   * The name of the qualifier. The name should be an element of a constrained vocabulary and is used to determine meaning of the qualifier.
+   *
+   * @param name The name of the qualifier.
+   */
+  public void setName(URI name) {
+    this.name = name;
+  }
+
+  /**
+   * Get the name as an element of a constrained vocabulary.
+   *
+   * @param vocabulary The enum containing the constrained vocabulary.
+   * @return The vocabulary element.
+   */
+  @XmlTransient
+  @JsonIgnore
+  @org.codehaus.enunciate.json.JsonIgnore
+  public <E extends Enum> E getName(Class<E> vocabulary) {
+    return this.name == null ? null : XmlQNameEnumUtil.fromURI(this.name.toURI(), vocabulary);
+  }
+
+  /**
+   * Set the qualifier name as an element of a constrained vocabulary.
+   *
+   * @param element The element.
+   */
+  @JsonIgnore
+  public void setName(Enum element) {
+    this.name = element == null ? null : new URI(XmlQNameEnumUtil.toURI(element).toString());
+  }
+
+  /**
+   * The value of the qualifier. Some qualifiers may not have values, indicating that the qualifier is to be treated more like a "tag".
+   *
+   * @return The value of the qualifier.
+   */
+  @XmlValue
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * The value of the qualifier. Some qualifiers may not have values, indicating that the qualifier is to be treated more like a "tag".
+   *
+   * @param value The value of the qualifier.
+   */
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
