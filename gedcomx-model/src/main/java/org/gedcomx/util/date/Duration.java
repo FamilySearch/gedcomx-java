39c61487f54f54ce5979ef316e3c3bcde473e985
Ryan Heaton <heatonra@familysearch.org>
2013-08-28 10:23:32 -0600

    annotating the date utility classes as appropriate

--- gedcomx-model/src/main/java/org/gedcomx/util/date/Duration.java
+++ gedcomx-model/src/main/java/org/gedcomx/util/date/Duration.java
@@ -1,5 +1,21 @@
+/**
+ * Copyright 2011-2012 Intellectual Reserve, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gedcomx.util.date;
 
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +31,7 @@ import java.util.regex.Pattern;
  * Date: 8/8/13
  * Time: 6:37 PM
  */
+@XmlTransient
 public class Duration {
   //  P[yyyyY][mmM][ddD][T[hhH][mmM][ssS]]
   private static final Pattern durationPattern = Pattern.compile("P(\\d{4}Y)?(\\d{2}M)?(\\d{2}D)?(?:T(\\d{2}H)?(\\d{2}M)?(\\d{2}S)?)?");
--- gedcomx-model/src/main/java/org/gedcomx/util/date/SimpleDate.java
+++ gedcomx-model/src/main/java/org/gedcomx/util/date/SimpleDate.java
@@ -1,5 +1,21 @@
+/**
+ * Copyright 2011-2012 Intellectual Reserve, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gedcomx.util.date;
 
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +25,7 @@ import java.util.regex.Pattern;
  * or, using square brackets around optional parts:
  *   (+|-)YYYY[-MM[-DD[Thh[:mm[:ss[(+\-)hh[:mm]|Z]]]]]]
  */
+@XmlTransient
 public class SimpleDate {
   private int year; // -9999 = 10000 B.C.; -1 = 2 B.C.; 0 = 1 B.C.; 1 = 1 A.D.; 9999 = 9999 A.D.
   private Integer month; // 1..12 = January..December; null => none
