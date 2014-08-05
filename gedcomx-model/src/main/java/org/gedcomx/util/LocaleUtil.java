510a0004e7514daa0c59ecb821fbcf65b53ade03
Ryan Heaton <heatonra@familysearch.org>
2014-08-05 11:32:57 -0600

    fix-ups to the record iterating utilities

--- gedcomx-model/src/main/java/org/gedcomx/util/LocaleUtil.java
+++ gedcomx-model/src/main/java/org/gedcomx/util/LocaleUtil.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright Intellectual Reserve, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gedcomx.util;
 
 import org.gedcomx.common.TextValue;
@@ -36,7 +51,7 @@ public class LocaleUtil {
       TextValue bestTextValue = null;
       Locale bestLocale = null;
       for (TextValue textValue : textValues) {
-        Locale locale = Locale.forLanguageTag(textValue.getLang());
+        Locale locale = new Locale(textValue.getLang());
         if (bestTextValue == null || LocaleUtil.isBetterLocaleMatch(localeToMatch, locale, bestLocale, defaultLocale)) {
           bestTextValue = textValue;
           bestLocale = locale;
