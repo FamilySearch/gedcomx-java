0d41f8184e27491b85966c573877540ca0d62a7b
Randy Wilson <wilsonr@familysearch.org>
2014-10-10 11:07:54 -0600

    add getPerson(ResourceReference)

--- gedcomx-model/src/main/java/org/gedcomx/util/DocMap.java
+++ gedcomx-model/src/main/java/org/gedcomx/util/DocMap.java
@@ -17,6 +17,7 @@ package org.gedcomx.util;
 
 import org.gedcomx.Gedcomx;
 import org.gedcomx.agent.Agent;
+import org.gedcomx.common.ResourceReference;
 import org.gedcomx.common.URI;
 import org.gedcomx.conclusion.Identifier;
 import org.gedcomx.conclusion.Person;
@@ -115,6 +116,16 @@ public class DocMap {
    */
   public Person getPerson(URI uri) {
     return uri == null ? null : getPerson(uri.toString());
+  }
+
+  /**
+   * Get the person from the GedcomX document that has the id given in the URI of the given ResourceReference.
+   * Can look a person up by "p1", "#p1" or "http://whatever.com/persons/12345".
+   * @param resourceReference - local person id (with or without "#") or any person identifier
+   * @return Person with the given id or identifier.
+   */
+  public Person getPerson(ResourceReference resourceReference) {
+    return resourceReference == null ? null : getPerson(resourceReference.getResource());
   }
 
   /**
