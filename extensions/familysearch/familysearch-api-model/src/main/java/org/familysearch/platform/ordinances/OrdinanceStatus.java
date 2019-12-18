72ed144056f3549ec9941eed7ced7f06f5058fb2
David Nash <NashDL@familysearch.org>
2019-12-18 08:49:13 -0700

    new Odinance Status types

--- extensions/familysearch/familysearch-api-model/src/main/java/org/familysearch/platform/ordinances/OrdinanceStatus.java
+++ extensions/familysearch/familysearch-api-model/src/main/java/org/familysearch/platform/ordinances/OrdinanceStatus.java
@@ -28,65 +28,53 @@ import org.gedcomx.rt.EnumURIMap;
 )
 public enum OrdinanceStatus implements ControlledVocabulary {
 
-  /**
-   * The ordinance has been completed.
-   */
+  /** The ordinance has been completed. */
   Completed,
 
-  /**
-   * The ordinance is currently in progress of the completion process by a different.
-   */
+  /** The ordinance is currently in progress of the completion process by a different user. */
   InProgressByOther,
 
-  /**
-   * The ordinance is currently in progress of the completion process by the current user and can be resubmitted/printed by the current user.
-   */
+  /** The ordinance has been reserved, shared with or assigned to Church inventory, and is currently in progress of the completion process by a different user. */
+  InProgressByOtherAfterShared,
+
+  /** The ordinance is currently in progress of the completion process by the current user and can be resubmitted/printed by the current user. */
   InProgressBySelf,
 
-  /**
-   * The ordinance can not be reserved by the current user because more information is needed about the person.
-   */
+  /** The ordinance can not be reserved by the current user because more information is needed about the person. */
   NeedMoreInformation,
 
-  /**
-   * The ordinance can not be reserved by the current user without special permission.
-   */
+  /** The ordinance can not be reserved by the current user without special permission. */
   NeedPermission,
 
-  /**
-   * The ordinance is not available to be reserved by the current user.
-   */
+  /** The ordinance is not available to be reserved by the current user. */
   NotAvailable,
 
-  /**
-   * The ordinance can not be reserved because it is not needed according to the policies of the Church.
-   */
+  /** The ordinance can not be reserved because it is not needed according to the policies of the Church. */
   NotNeeded,
 
-  /**
-   * The ordinance can not be reserved because it is not needed because the person was born in the covenant.
-   */
+  /** The ordinance can not be reserved because it is not needed because the person was born in the covenant. */
   NotNeededBornInCovenant,
 
-  /**
-   * The ordinance can not currently be reserved by the current user, but it is expected that the ordinance will eventually become <code>Ready</code> after a period of time.
-   */
+  /** The ordinance can not currently be reserved by the current user, but it is expected that the ordinance will eventually become <code>Ready</code> after a period of time. */
   NotReady,
 
-  /**
-   * The ordinance can be reserved by the current user.
-   */
+  /** The ordinance can be reserved by the current user. */
   Ready,
 
-  /**
-   * The ordinance has been reserved by a different user.
-   */
+  /** The ordinance has been reserved by a different user. */
   ReservedByOther,
 
-  /**
-   * The ordinance has been reserved by the current user and can be submitted/printed.
-   */
+  /** The ordinance has been reserved by a different user and shared with or assigned to Church inventory. */
+  ReservedByOtherAndSharedReady,
+
+  /** The ordinance has been reserved by the current user and can be submitted/printed. */
   ReservedBySelf,
+
+  /** The ordinance has been reserved by the current user and shared with or assigned to Church inventory. */
+  ReservedBySelfAndShared,
+
+  /** The ordinance has been reserved by the current user and is waiting for prerequisite ordinances to be completed. */
+  ReservedBySelfAndWaiting,
 
   @XmlUnknownQNameEnumValue
   OTHER;
