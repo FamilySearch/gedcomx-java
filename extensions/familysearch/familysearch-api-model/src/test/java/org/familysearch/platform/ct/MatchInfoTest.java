bee540ea08e26ad43c182f1de9a2c1020c54dce1
Travis Nelson <tjnelson@familysearch.org>
2025-04-28 10:13:02 -0600

    Add OpenAPI schema annotations for additional match attributes

--- extensions/familysearch/familysearch-api-model/src/test/java/org/familysearch/platform/ct/MatchInfoTest.java
+++ extensions/familysearch/familysearch-api-model/src/test/java/org/familysearch/platform/ct/MatchInfoTest.java
@@ -21,6 +21,23 @@ public class MatchInfoTest {
     assertNull(matchInfo.getAddsFact());
     assertNull(matchInfo.getAddsDateOrPlace());
     assertNull(matchInfo.getHasFourOrMorePeople());
+    assertNull(matchInfo.getAddsFather110YearRule());
+    assertNull(matchInfo.getAddsMother110YearRule());
+    assertNull(matchInfo.getAddsParentUnknownGender110YearRule());
+    assertNull(matchInfo.getAddsSpouse110YearRule());
+    assertNull(matchInfo.getAddsSon110YearRule());
+    assertNull(matchInfo.getAddsDaughter110YearRule());
+    assertNull(matchInfo.getAddsChildUnknownGender110YearRule());
+    assertNull(matchInfo.getAddsBirth());
+    assertNull(matchInfo.getAddsChristening());
+    assertNull(matchInfo.getAddsDeath());
+    assertNull(matchInfo.getAddsBurial());
+    assertNull(matchInfo.getAddsMarriage());
+    assertNull(matchInfo.getAddsOtherFact());
+    assertNull(matchInfo.getAddsDate());
+    assertNull(matchInfo.getAddsPlace());
+    assertNull(matchInfo.getImprovesDate());
+    assertNull(matchInfo.getImprovesPlace());
 
     matchInfo.setKnownCollection(MatchCollection.tree);
     matchInfo.setKnownStatus(MatchStatus.Accepted);
@@ -29,6 +46,23 @@ public class MatchInfoTest {
     matchInfo.setAddsFact(true);
     matchInfo.setAddsDateOrPlace(true);
     matchInfo.setHasFourOrMorePeople(true);
+    matchInfo.setAddsFather110YearRule(true);
+    matchInfo.setAddsMother110YearRule(true);
+    matchInfo.setAddsParentUnknownGender110YearRule(true);
+    matchInfo.setAddsSpouse110YearRule(true);
+    matchInfo.setAddsSon110YearRule(true);
+    matchInfo.setAddsDaughter110YearRule(true);
+    matchInfo.setAddsChildUnknownGender110YearRule(true);
+    matchInfo.setAddsBirth(true);
+    matchInfo.setAddsChristening(true);
+    matchInfo.setAddsDeath(true);
+    matchInfo.setAddsBurial(true);
+    matchInfo.setAddsMarriage(true);
+    matchInfo.setAddsOtherFact(true);
+    matchInfo.setAddsDate(true);
+    matchInfo.setAddsPlace(true);
+    matchInfo.setImprovesDate(true);
+    matchInfo.setImprovesPlace(true);
 
     assertEquals(MatchCollection.tree.toQNameURI(), matchInfo.getCollection());
     assertEquals(MatchStatus.Accepted.toQNameURI(), matchInfo.getStatus());
@@ -37,6 +71,23 @@ public class MatchInfoTest {
     assertTrue(matchInfo.getAddsFact());
     assertTrue(matchInfo.getAddsDateOrPlace());
     assertTrue(matchInfo.getHasFourOrMorePeople());
+    assertTrue(matchInfo.getAddsFather110YearRule());
+    assertTrue(matchInfo.getAddsMother110YearRule());
+    assertTrue(matchInfo.getAddsParentUnknownGender110YearRule());
+    assertTrue(matchInfo.getAddsSpouse110YearRule());
+    assertTrue(matchInfo.getAddsSon110YearRule());
+    assertTrue(matchInfo.getAddsDaughter110YearRule());
+    assertTrue(matchInfo.getAddsChildUnknownGender110YearRule());
+    assertTrue(matchInfo.getAddsBirth());
+    assertTrue(matchInfo.getAddsChristening());
+    assertTrue(matchInfo.getAddsDeath());
+    assertTrue(matchInfo.getAddsBurial());
+    assertTrue(matchInfo.getAddsMarriage());
+    assertTrue(matchInfo.getAddsOtherFact());
+    assertTrue(matchInfo.getAddsDate());
+    assertTrue(matchInfo.getAddsPlace());
+    assertTrue(matchInfo.getImprovesDate());
+    assertTrue(matchInfo.getImprovesPlace());
   }
 
 }
